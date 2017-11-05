88aadd2098de2589ef273a4d823cea77b0d913d0
DEBOTOSH SARKAR <debotoshsarkar44@gmail.com>
2017-11-05 22:54:35 +0530

    Time table generation management system
    I have used Java and Genetic Algorithm

---
+++ dynamicTT/src/dynamicTT/TimeTableMain.java
@@ -0,0 +1,16 @@
+package dynamicTT;
+
+import java.io.IOException;
+
+
+
+public class TimeTableMain {
+
+	
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("Start.......");
+	        Initialization initialize=new Initialization();       
+	        initialize.readInput();
+	}
+}
